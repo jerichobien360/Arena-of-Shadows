5f42bdbb0a940b9e4537e0b6e3814f3b66641204
Jericho Bien <jerichobien360@gmail.com>
2025-06-12 11:42:30 +0800

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,16 @@
+
+
+# Constants
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 768
+FPS = 60
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+GRAY = (128, 128, 128)
+DARK_GRAY = (64, 64, 64)
+YELLOW = (255, 255, 0)

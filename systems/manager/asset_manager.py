8f399775e3ae59515849b0ed5532bf2e00f07b29
Jericho Bien <jerichobien360@gmail.com>
2025-06-21 21:36:19 +0800

    Update asset_manager.py

--- systems/manager/asset_manager.py
+++ systems/manager/asset_manager.py
@@ -1,11 +1,23 @@
+from systems.manager.decorator_manager import *
 import pygame
 
 def image_load(file_path):
+    '''This will be used to import the image/sprite'''
     try:
-        icon = pygame.image.load(file_path)
+        icon = pygame.image.load(file_path).convert_alpha()
         pygame.display.set_icon(icon)
         return icon
     except pygame.error:
-        pass  # Continue without icon if file not found
+        error_message(file_path, name="File")
+        pass
 
     return
+
+def create_font(file_path, size=12):
+    try:
+        font = pygame.font.Font(file_path, 18)
+    except pygame.error:
+        error_message(file_path, name="Font")
+        pass
+
+    return font

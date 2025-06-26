25c868590e6d7b882cc00aec601b62a8c4860e5a
Jericho Bien <jerichobien360@gmail.com>
2025-06-26 22:10:53 +0800

    Update asset_manager.py

--- systems/manager/asset_manager.py
+++ systems/manager/asset_manager.py
@@ -1,12 +1,23 @@
 from systems.manager.decorator_manager import *
 import pygame
 
-def image_load(file_path):
+def image_icon_load(file_path):
     '''This will be used to import the image/sprite'''
     try:
         icon = pygame.image.load(file_path).convert_alpha()
         pygame.display.set_icon(icon)
         return icon
+    except pygame.error:
+        error_message(file_path, name="File")
+        pass
+
+    return
+
+def image_background_load(file_path, SCREEN_WIDTH, SCREEN_HEIGHT):
+    '''This will be used to import the resized background onto the screen'''
+    try:
+        background = pygame.image.load(file_path)
+        return pygame.transform.scale(background,(SCREEN_WIDTH, SCREEN_HEIGHT))
     except pygame.error:
         error_message(file_path, name="File")
         pass

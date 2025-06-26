ae94a5a116df6d17ad05dde73699161e9877506a
Jericho Bien <jerichobien360@gmail.com>
2025-06-27 02:12:44 +0800

    Create pause_menu_screen.py

---
+++ ui/screens/pause_menu_screen.py
@@ -0,0 +1,20 @@
+"""
+    Pause Menu - This will be used for modeling layout from the gameplay_core.py
+"""
+
+
+class Layout:
+    '''Parent-Abstraction to be used for every different layout of this game '''
+    def __init__(self):
+        pass
+        
+    def __del__(self):
+        pass
+
+class Pause:
+    '''Abstraction for establishing the ideal pause layout '''
+    def __init__(self):
+        pass
+    
+    def __del__(self):
+        pass

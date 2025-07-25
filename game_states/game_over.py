37e729306f15fda421e0caa2317fec4cd53ee72e
Jericho Bien <jerichobien360@gmail.com>
2025-07-25 13:49:46 +0800

    Update game_over.py

--- game_states/game_over.py
+++ game_states/game_over.py
@@ -1,4 +1,5 @@
 from settings import *
+from game_function.game_function import *
 import pygame
 from game_states.gameplay import GameState
 from ui.screens.game_over_screen import GameOverVisuals
@@ -16,11 +17,13 @@ class GameOverState(GameState):
     # -------------------INITIALIZE & CLEANUP-----------------------------
     def enter(self):
         """Initialize state when entering game over"""
+        DEBUGGING('GAME_OVER_INIT', DEBUGGING_ENABLE)
         self.sound_manager.stop_background_music()
         self._reset_state()
         self.visuals.reset()
     
-    def exit(self): # TODO: Create an exit function from line of code 49-50
+    def exit(self): # TODO: Create an exit function from line of "if self.can_exit and self._check_input():"
+        DEBUGGING('GAME_OVER_EXIT', DEBUGGING_ENABLE)
         pass
 
     # -------------------CLASS METHOD-------------------------------------
@@ -51,6 +54,7 @@ class GameOverState(GameState):
         
         # Handle input
         if self.can_exit and self._check_input():
+            print("Return to Main Menu Screen...")
             return "main_menu"
         
         return None
